4858b2fae5bcc6a6d847cd1e23544d696935d6af
KwameSei <nathanielosei92@gmail.com>
2023-11-14 15:44:54 +0000

    Home component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Home } from './components';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
 function App() {
 
   return (
-    <div>
+    <div className='app'
+    >
+      <ToastContainer
+          position='top-right'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={true}
+          rtl={false}
+          pauseOnFocusLoss={true}
+          draggable={true}
+          pauseOnHover={true}
+          theme='colored'
+        />
+
       <Home />
     </div>
   )
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getLocation = async (code) => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.get(`https://api.zippopotam.us/in/${code}`)
+      console.log('Response is: ', response)
+
+      const responseData = response.data;
+      console.log('Response Data is: ', responseData)
+
+      setLocation(responseData);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <div>This is a Home component</div>
   )
